357da909bd8e9f6b313324ed33f4be8f2f7d5115
Matrix-Monk <matrixmonk22@gmail.com>
2025-03-30 00:07:56 +0530

    Fixed Participent list

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -55,9 +55,7 @@ const MeetingRoom = () => {
           <CallLayout />
         </div>
         <div
-          className={cn("h-[calc(100vh-86px)] hidden ml-2", {
-            "show-block": showParticipants,
-          })}
+          className={cn("h-[calc(100vh-86px)] w-75 ml-2",showParticipants ? "block opacity-100" : "hidden opacity-0")}
         >
           <CallParticipantsList onClose={() => setShowParticipants(false)} />
         </div>
@@ -88,7 +86,7 @@ const MeetingRoom = () => {
           </DropdownMenuContent>
         </DropdownMenu>
         <CallStatsButton />
-        
+
         <button onClick={() => setShowParticipants((prev) => !prev)}>
           <div className=" cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
             <Users size={20} className="text-white" />
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -69,7 +69,7 @@ const MeetingTypeList = () => {
 
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
-      <HomeCard
+       <HomeCard
         img="icons/add-meeting.svg"
         title="New Meeting"
         description="Start an instant meeting"
